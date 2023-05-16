a18fe21b726bd295d1e5285fd8dd29194ee38922
alexitoo00 <47087779+alexitoo00@users.noreply.github.com>
2023-05-16 10:50:50 +0200

    Code optimized and commented

--- examen_10_Mayo_2022_a.py
+++ examen_10_Mayo_2022_a.py
@@ -5,67 +5,59 @@ def CrearMatriz(max_i, max_j):
     i = 0
     j = 0
     while True:
-        if i == max_i:
-            j += 1
-            i = 0
-            if j == 5:
-                break
-            if j == max_j:
-                break
-            matriz.append([])
-            continue
-        if j == 5:
-            break
-        print("Posición ", i, " ", j)
-        val = input("Mete números entre 1 e 10: ")
-        try:
+        if i == max_i:                              # Si se chega ao final da columna
+            j += 1                                  # - Pásase á siguiente
+            i = 0                                   # - Reiníciase o iterador de filas
+            if j == max_j:                          # Si se chega á última columna
+                break                               # - Terminamos a función
+            matriz.append([])                       # Añadimos unha nova columna
+
+        print("Posición [", i, " ", j, "]")         # Pedimos un valor
+        val = input("Mete un valor entre 1 e 10: ")
+
+        try:                                        # Checkeamos si é un float
             val = float(val)
         except:
             print("Error")
             continue
-        if val < 1 or val > 10:
+
+        if val < 1 or val > 10:                     # Comprobamos que estea entre 1 e 10
             print("Error")
             continue
-        matriz[j].append(val)
-        i += 1
-        if i == 5:
-            j += 1
-            i = 0
-            if j == 5:
-                break
-            matriz.append([])
-            continue
 
-    return matriz
+        matriz[j].append(val)                       # Añadimos o valor á columna correspondiente
+        i += 1
+
+    return matriz                                   # Devolvemos o objeto matriz
 
 
 def menu():
     global matriz1
     global matriz2
 
-    while True:
-        print("0 - Definir")
-        print("1 - Sumar diagonales")
+    while True:                                     # Definir lista de opcións
+        print("0 - Definir matrices")
+        print("1 - Sumar diagonales principales")
         print("2 - Sumar matrices")
-        print("3 - Visualizar")
+        print("3 - Visualizar matrices")
         print("4 - Salir")
         msg = input("Escolle unha opción: ")
-        try:
+        try:                                        # Comprobar que se introducise un número de opción válido
             num = int(msg)
             if num < 0 or num > 4:
                 print("Escribe un número entre 0 e 4")
                 continue
-            if num == 0:
+            if num == 0:                            # Opción 0
                 DefinirMatrices()
-            if num == 1:
+            if num == 1:                            # Opción 1
                 SumarDiagonales(matriz1)
                 SumarDiagonales(matriz2)
-            if num == 2:
+            if num == 2:                            # Opción 2
                 SumarMatrices(matriz1, matriz2)
-            if num == 3:
+            if num == 3:                            # Opción 3
                 VerMatriz(matriz1)
                 VerMatriz(matriz2)
-            if num == 4:
+            if num == 4:                            # Opción 4
                 print("Bye!")
                 break
         except:
@@ -81,7 +73,7 @@ matriz2 = []
 def DefinirMatrices():
     global matriz1
     global matriz2
-    matriz1 = DefinirMatriz("Matriz 1")
+    matriz1 = DefinirMatriz("Matriz 1")             # A efectos meramente organizativos
     matriz2 = DefinirMatriz("Matriz 2")
     print()
 
@@ -89,13 +81,13 @@ def DefinirMatrices():
 def DefinirMatriz(nombre):
     print(nombre)
     try:
-        i = int(input("Rango i (max 5): "))
+        i = int(input("Rango i (max 5): "))         # Pídese o rango da matriz (filas e columnas)
         j = int(input("Rango j (max 5): "))
-        if i < 1 or i > 5:
+        if i < 1 or i > 5:                          # Compróbase que estean entre 1 e 5
             return
         if j < 1 or j > 5:
             return
-        matriz = CrearMatriz(i, j)
+        matriz = CrearMatriz(i, j)                  # Créase a matriz có rango solicitado
         return matriz
     except:
         print("Error")
@@ -105,24 +97,24 @@ def DefinirMatriz(nombre):
 def SumarDiagonales(matriz):
     sumPrinc = 0
     sumSec = 0
-    iter = min(len(matriz), len(matriz[0]))
+    iter = min(len(matriz), len(matriz[0]))         # Calculamos o valor mínimo entre filas e columnas
     for i in range(iter):
-        sumPrinc += matriz[i][i]
-        sumSec += matriz[i][iter-i-1]
+        sumPrinc += matriz[i][i]                    # Diagonal principal
+        sumSec += matriz[i][iter-i-1]               # Diagonal secundaria
     print("Suma diagonal principal: ", sumPrinc)
     print("Suma diagonal secundaria: ", sumSec)
 
 
 def SumarMatrices(matriz_a, matriz_b):
     if len(matriz_a) != len(matriz_b) or len(matriz_a[0]) != len(matriz_b[0]):
-        print("Matrices de distinto rango")
-        return ValueError
-    resultado = [[]]
+        print("Matrices de distinto rango")         # Compróbase que ambas matrices teñan o mismo número
+        return ValueError                           #  de filas e de columnas
+    
+    resultado = []
     for i in range(len(matriz_a)):
-        for j in range(len(matriz_a[0])):
+        resultado.append([])                        # Añade unha columna de cada vez iterando por todas as columnas
+        for j in range(len(matriz_a[0])):           # Itera por todas as filas
             resultado[i].append(matriz_a[i][j] + matriz_b[i][j])
-        if i != len(matriz_a)-1:
-            resultado.append([])
     print(resultado)
 
 
@@ -130,4 +122,4 @@ def VerMatriz(matriz):
     print(matriz)
 
 
-menu()
+menu()                                              # Inicio do programa
